54cd28ab167e57a63d9e19edc2581357510500a1
md-nasim-mondal <nasim.programmer24@gmail.com>
2025-07-30 14:06:46 +0600

    used useDispatch and made two types in store and made a two hook with this type and also made handle Increment Decrement functions with implement it

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
+import { decrement, increment } from "./redux/features/counter/counterSlice";
+import { useAppDispatch, useAppSelector } from "./redux/hook";
+
 function App() {
+  const dispatch = useAppDispatch();
+  const { count } = useAppSelector((state) => state.counter);
+
+  const handleIncrement = () => {
+    dispatch(increment());
+  };
+
+  const handleDecrement = () => {
+    dispatch(decrement());
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen space-y-4">
-      <h1 className="text-2xl font-bold mb-4">Counter with Redux</h1>
-      <div className="flex space-x-4">
-      <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Increment</button>
-      <div className="text-xl font-semibold">0</div>
-      <button className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600">Decrement</button>
+    <div className='flex flex-col items-center justify-center min-h-screen space-y-4'>
+      <h1 className='text-2xl font-bold mb-4'>Counter with Redux</h1>
+      <div className='flex space-x-4'>
+        <button
+          onClick={() => handleIncrement()}
+          className='px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600'>
+          Increment
+        </button>
+        <div className='text-xl font-semibold'>{count}</div>
+        <button
+          onClick={() => handleDecrement()}
+          className='px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600'>
+          Decrement
+        </button>
       </div>
     </div>
   );
